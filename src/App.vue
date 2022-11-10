390b8230c35aaeb5ac9a3dfd2beba09ea842ed51
Quentin Burgniard <quentinburgniard@gmail.com>
2022-11-10 14:03:13 +0100

    feat(paragraph)

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,74 @@
 <script setup>
-  import { ref, watch } from 'vue';
-  import Login from './components/Login.vue'
-  let token = ref(localStorage.getItem('token') || '');
+  import { computed, ref, watch } from 'vue';
+  import Paragraph from './components/Paragraph.vue';
+  let text = ref('');
+  let validated = ref(false);
 
-  function login (jwt) {
-    token.value = jwt;
+  function createParagraph() {
+    if (text.value) {
+      fetch('https://api.digitalleman.com/v2/paragraphs', {
+        body: JSON.stringify({
+          text: text.value
+        }),
+        headers: {
+          'authorization': `Bearer ${token()}`,
+          'content-type': 'application/json'
+        },  
+        method: 'POST'
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          redirect();
+        }
+      });   
+    } else {
+      validated.value = true;
+    }
   }
 
-  watch(token, async (newToken) => {
-    document.cookie = 't=' + newToken + ';domain=' + location.hostname.match(/[^.]+[.][^.]+$/)[0] + ';samesite=strict;secure';
-    localStorage.setItem('t', newToken);
-    let redirect = new URLSearchParams(document.location.search).get('r');
-    if (redirect) window.location.replace(redirect + '?t=' + newToken);
-  })
+  function getParagraphs() {
+    fetch('https://api.digitalleman.com/v2/paragraphs', {
+      headers: {
+        'authorization': `Bearer ${token()}`,
+        'content-type': 'application/json'
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data.error) {
+        //redirect();
+      }
+    });
+  }
+
+  function redirect() {
+    window.location.href = 'https://id.digitalleman.com?r=127.0.0.1%3A5173';
+  }
+
+  function token() {
+    let match = document.cookie.match(/t=([^;]+)/);
+    if (match && match[1]) {
+      return match[1];
+    } else {
+      redirect();
+    }
+  }
 </script>
 
 <template>
-  <Login v-if="token == ''" @login="login"/>
+  <Paragraph/>
+  <form class="needs-validation" :class="{ 'was-validated': validated }" novalidate @submit.prevent="createParagraph">
+    <div class="mt-3">
+      <label class="form-label" for="text">Texte</label>
+      <textarea class="form-control" id="text" placeholder="Paragraphe" required v-model.lazy="text"></textarea>
+      <div class="invalid-feedback">
+        Texte est requis.
+      </div>
+    </div>
+    <div class="mt-3">
+      <button class="btn btn-primary" type="submit">Ajouter un paragraphe</button>
+    </div>
+  </form>
 </template>
---
+++ src/components/Paragraph.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import { ref } from 'vue';
+  const emit = defineEmits();
+  let selected = ref(false);
+  let text = ref('');
+</script>
+
+<template>
+  <div class="card" :class="{ 'border-primary': selected }" @click="selected = true">
+    <div class="card-body" :class="{ 'text-primary': selected }">
+      {{ text }}
+    </div>
+  </div>
+</template>
